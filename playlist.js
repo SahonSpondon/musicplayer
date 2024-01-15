c3a33c0aa64b117f00730da3675018bf39bbf87f
Sahon Spondon <108812194+SahonSpondon@users.noreply.github.com>
2024-01-15 22:06:52 +0600

    Add files via upload

---
+++ playlist.js
@@ -0,0 +1,60 @@
+let songs = [
+  {
+    name: "One Love",
+    artist: "Shubh",
+    img: "img1",
+    audio: "music1"
+  },
+  {
+    name: "Alag Aasman",
+    artist: "Anuv Jain",
+    img: "img2",
+    audio: "music2"
+  },
+  {
+    name: "Choo lo",
+    artist: "The Local Train",
+    img: "img3",
+    audio: "music3"
+  },
+  {
+    name: "Starboy",
+    artist: "The Weekend",
+    img: "img4",
+    audio: "music4"
+  },
+  {
+    name: "Ei Obelay",
+    artist: "Shironamhin",
+    img: "img5",
+    audio: "music5"
+  }
+  ,
+  {
+    name: "Bedona",
+    artist: "Shunno",
+    img: "img6",
+    audio: "music6"
+  }
+  ,
+  {
+    name: "One dance",
+    artist: "Drake",
+    img: "img7",
+    audio: "music7"
+  }
+  ,
+  {
+    name: "Moho",
+    artist: "Aftermath",
+    img: "img8",
+    audio: "music8"
+  }
+  ,
+  {
+    name: "Tu hai kahan",
+    artist: "AUR",
+    img: "img9",
+    audio: "music9"
+  },
+]
